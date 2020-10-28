4b55a5c11db423008f498855219525f6af8444bb
Grahame White <4whitg16@solent.ac.uk>
2020-10-28 16:02:39 +0000

    added all functionality up to the end of task 3

---
+++ models/Taster.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const tasterSchema = new Schema(
+    {
+        twitter: String,
+        tastings: Number,
+        name: String,
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Tasters", tasterSchema);
