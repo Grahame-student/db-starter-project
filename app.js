473abf1333382c6bf3a2009546e5a5083be7f8b1
Grahame White <4whitg16@solent.ac.uk>
2020-12-02 15:43:31 +0000

    Removed double connect

--- app.js
+++ app.js
@@ -21,32 +21,9 @@ const app = express();
 app.set("view engine", "ejs");
 
 const mongoose = require("mongoose");
-const {WEB_PORT, MONGODB_URI} = process.env;
+const {PORT, MONGODB_URI} = process.env;
 
 const tasterController = require("./controllers/taster");
-
-mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-  console.log(
-      "MongoDB connection error. Please make sure MongoDB is running.",
-  );
-  process.exit();
-})
-
-/**
- * notice above we are using dotenv. We can now pull the values from our environment
- */
-
-const { PORT, MONGODB_URI } = process.env;
-
-/**
- * connect to database
- */
-
-
-
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.on("error", (err) => {
