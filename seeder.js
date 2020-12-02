be25f0fca056eb05142add3daf0a99ae8a859154
Grahame White <4whitg16@solent.ac.uk>
2020-12-02 16:10:17 +0000

    Applied formatting to address lint comments

--- seeder.js
+++ seeder.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
-const fs = require("fs").promises;
-const path = require("path");
-const loading = require("loading-cli");
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const fs = require('fs').promises;
+const path = require('path');
+const loading = require('loading-cli');
 
 /**
  * constants
@@ -14,7 +14,7 @@ async function main() {
   try {
     await client.connect();
     const db = client.db();
-    const results = await db.collection("tastings").find({}).count();
+    const results = await db.collection('tastings').find({}).count();
 
     /**
      * If existing records then delete the current collections
@@ -27,14 +27,14 @@ async function main() {
      * This is just a fun little loader module that displays a spinner
      * to the command line
      */
-    const load = loading("importing your wine 🍷!!").start();
+    const load = loading('importing your wine 🍷!!').start();
 
     /**
      * Import the JSON data into the database
      */
 
-    const data = await fs.readFile(path.join(__dirname, "wine.json"), "utf8");
-    await db.collection("tastings").insertMany(JSON.parse(data));
+    const data = await fs.readFile(path.join(__dirname, 'wine.json'), 'utf8');
+    await db.collection('tastings').insertMany(JSON.parse(data));
 
     /**
      * This perhaps appears a little more complex than it is. Below, we are
@@ -42,12 +42,12 @@ async function main() {
      * we tidy up the output so it represents the format we need for our new collection
      */
 
-    const wineTastersRef = await db.collection("tastings").aggregate([
+    const wineTastersRef = await db.collection('tastings').aggregate([
       { $match: { taster_name: { $ne: null } } },
       {
         $group: {
-          _id: "$taster_name",
-          twitter: { $first: "$taster_twitter_handle" },
+          _id: '$taster_name',
+          twitter: { $first: '$taster_twitter_handle' },
           tastings: { $sum: 1 },
         },
       },
@@ -65,22 +65,22 @@ async function main() {
      * new collection
      */
     const wineTasters = await wineTastersRef.toArray();
-    await db.collection("tasters").insertMany(wineTasters);
+    await db.collection('tasters').insertMany(wineTasters);
 
     /** This data manipulation is to reference each document in the
      * tastings collection to a taster id. Further to this we also take the opportunity to
      * tidy up points (converting it to a int) and regions, adding them to a an array
      */
 
-    const updatedWineTastersRef = db.collection("tasters").find({});
+    const updatedWineTastersRef = db.collection('tasters').find({});
     const updatedWineTasters = await updatedWineTastersRef.toArray();
     updatedWineTasters.forEach(async ({ _id, name }) => {
-      await db.collection("tastings").updateMany({ taster_name: name }, [
+      await db.collection('tastings').updateMany({ taster_name: name }, [
         {
           $set: {
             taster_id: _id,
-            regions: ["$region_1", "$region_2"],
-            points: { $toInt: "$points" },
+            regions: ['$region_1', '$region_2'],
+            points: { $toInt: '$points' },
           },
         },
       ]);
@@ -91,48 +91,48 @@ async function main() {
      * placed them in an array
      */
     await db
-      .collection("tastings")
-      .updateMany({}, { $unset: { region_1: "", region_2: " " } });
+      .collection('tastings')
+      .updateMany({}, { $unset: { region_1: '', region_2: ' ' } });
 
     /**
      * Finally, we remove nulls regions from our collection of arrays
      * */
     await db
-      .collection("tastings")
+      .collection('tastings')
       .updateMany({ regions: { $all: [null] } }, [
-        { $set: { regions: [{ $arrayElemAt: ["$regions", 0] }] } },
+        { $set: { regions: [{ $arrayElemAt: ['$regions', 0] }] } },
       ])
 
-    db.collection("tastings").aggregate([
-      { $group: { _id: "$variety" } },
-      { $project: { name: "$_id", "_id": 0 } },
-      { $out: "varieties" }
+    db.collection('tastings').aggregate([
+      { $group: { _id: '$variety' } },
+      { $project: { name: '$_id', '_id': 0 } },
+      { $out: 'varieties' }
     ]).toArray();
 
-    db.collection("tastings").aggregate([
-      { $group: { _id: "$country" } },
-      { $project: { name: "$_id", "_id": 0 } },
-      { $out: "countries" }
+    db.collection('tastings').aggregate([
+      { $group: { _id: '$country' } },
+      { $project: { name: '$_id', '_id': 0 } },
+      { $out: 'countries' }
     ]).toArray()
 
-    await db.collection("tastings").aggregate([
-      { $group: { _id: "$province" } },
-      { $project: { name: "$_id", "_id": 0 } },
-      { $out: "provinces" }
+    await db.collection('tastings').aggregate([
+      { $group: { _id: '$province' } },
+      { $project: { name: '$_id', '_id': 0 } },
+      { $out: 'provinces' }
     ]).toArray()
 
-    await db.collection("tastings").aggregate([
-      { $unwind: "$regions" },
-      { $group: { _id: "$regions" } },
+    await db.collection('tastings').aggregate([
+      { $unwind: '$regions' },
+      { $group: { _id: '$regions' } },
       { $project: { name: '$_id', _id: 0 } },
-      { $out: "regions" }
+      { $out: 'regions' }
     ]).toArray();
 
-    await db.collection("tastings").aggregate([
-      { $unwind: "$regions" },
-      { $group: { _id: "$regions" } },
-      { $project: { name: "$_id", "_id": 0 } },
-      { $out: "regions" }
+    await db.collection('tastings').aggregate([
+      { $unwind: '$regions' },
+      { $group: { _id: '$regions' } },
+      { $project: { name: '$_id', '_id': 0 } },
+      { $out: 'regions' }
     ]).toArray()
 
     load.stop();
@@ -142,7 +142,7 @@ async function main() {
 
     process.exit();
   } catch (error) {
-    console.error("error:", error);
+    console.error('error:', error);
     process.exit();
   }
 }
